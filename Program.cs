41c4af83f00fd15b937442b0d2586467a868dab4
Alex Momotyuk <o.a.momotyuk@gmail.com>
2019-11-20 22:35:35 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_07.LINQ.Other_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# NSS.C-Sharp.1-07.LINQ.Other_Operations
